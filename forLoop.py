95b240e8323dbfcb1434b26f00fa23c40cbaf201
Sathmi Rajapaksha <devsadini@gmail.com>
2024-03-13 14:06:12 +0530

    first commit

---
+++ forLoop.py
@@ -0,0 +1,19 @@
+for x in "university":
+    print(x)
+
+print()
+
+for x in range(5):
+    print(x)
+
+print()
+
+for x in range(2,9):
+    print(x)
+
+print()
+
+
+###range(start,end,increment)
+for x in range(1,10,2):
+    print(x)
---
+++ whileLoop.py
@@ -0,0 +1,15 @@
+i=0
+while i<10:
+    if(i==5):
+        break
+    print(i)
+    i=i+1
+
+print()
+
+j=0
+while j<10:
+    j=j+1
+    if(j==5):
+        continue
+    print(j)
